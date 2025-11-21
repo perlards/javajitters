f911f337486a38b19d5177b4de6c321be43a2dc2
perla ruiz <perlards@bu.edu>
2025-11-21 12:40:59 -0500

    established main components for app skeleton

---
+++ types/CoffeeProps.ts
@@ -0,0 +1,11 @@
+export type CoffeeProps = {
+    id:string;
+    shopName:string;
+    coffeeType: string;
+    rating: number;
+    review: string;
+    picture: string;
+    category: string;
+    location: string;
+
+}
