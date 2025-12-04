3bd816e1e1eebc616b1182975fc0ba1207035367
Sofia Ford <scfordw@bu.edu>
2025-12-04 14:47:42 -0500

    Adding functionality to Nav to have log in link change with session

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,8 +1,22 @@
 "use client";
 
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 export default function Nav() {
+    const {data, status} = useSession();
+    let loggedIn = false;
+
+
+    if (status === "loading") {
+        loggedIn = false;
+    } else if (status === "unauthenticated") {
+        loggedIn = false;
+    } else if (status === "authenticated") {
+        loggedIn = true;
+    }
+
+
     return(
         <nav>
             <ul>
@@ -11,7 +25,7 @@ export default function Nav() {
                 <li><Link href={""}>Taste Test</Link></li>
                 <li><Link href={"/search"}>Search</Link></li>
                 <li><Link href={"/profile"}>Profile</Link></li>
-                <li><Link href={"/login"}>Log In</Link></li>
+                <li><Link href={"/login"} >{loggedIn ? "Log Out" : "Log In"}</Link></li>
             </ul>
         </nav>
     )
