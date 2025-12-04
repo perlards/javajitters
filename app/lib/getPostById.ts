d7e6c8ff04bc7b743151c4413fdee0fec5521933
Sofia Ford <scfordw@bu.edu>
2025-12-04 12:22:04 -0500

    Creating Typescript function getPostById to get post by individual id for full page functionality

---
+++ app/lib/getPostById.ts
@@ -0,0 +1,35 @@
+import getCollection, { POST_COLLECTION } from "...";
+import { ObjectId } from "mongodb";
+import { CoffeeProps } from "@/types/CoffeeProps";
+
+export default async function getPostById(id: string): Promise<CoffeeProps | null>{
+
+    const postCollection = await getCollection(POST_COLLECTION);
+
+    let objectId;
+
+    try{
+        objectId = new ObjectId(id);
+    }catch{
+        return null;
+    }
+
+    const data = await postCollection.findOne({_id: objectId});
+
+    if (data === null){
+        return null;
+    }
+
+    const coffeePost = {
+        id: data._id.toString(),
+        shopName: data.shopName,
+        coffeeType: data.coffeeType,
+        rating: data.rating,
+        review: data.review,
+        picture: data.picture,
+        category: data.category,
+        location: data.location,
+    }
+
+    return coffeePost;
+}
