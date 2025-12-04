c4498213fbdecdfd90e430de48000e23a66987f4
Sofia Ford <scfordw@bu.edu>
2025-12-04 12:30:53 -0500

    Writing code for FullCoffeePost page

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,1 +1,22 @@
+import FullCoffeePost from "@/app/components/FullCoffeePost";
+import getPostById from "@/app/lib/getPostById";
+import { redirect } from "next/navigation";
 
+export default async function FullPostPage({params}:{params:Promise<{id:string}>}){
+    const {id} = await params;
+
+    let post = null;
+
+    try{
+        post = await getPostById(id);
+    }catch(error){
+        console.log(error);
+        redirect("/");
+    }
+
+    if(!post){
+        redirect("/");
+    }
+
+    return <FullCoffeePost post={post}/>
+}
