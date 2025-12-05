12be4f614f6e0d8268c8ace9f30816ae543be745
Sofia Ford <scfordw@bu.edu>
2025-12-05 16:22:27 -0500

    Editing styling on Post

--- app/components/PostDisplay.tsx
+++ app/components/PostDisplay.tsx
@@ -5,11 +5,12 @@ import CoffeePost from "@/app/components/CoffeePost";
 
 export default function PostDisplay({inputPosts}:{inputPosts:CoffeeProps[]}){
     const posts = inputPosts;
+    const styleDiv = "flex flex-col items-center "
 
 
     return (
 
-        <div className={"flex flex-col items-center"}>
+        <div className={styleDiv}>
             {posts.map((p) =>
                 <CoffeePost key={p.id} post={p}/>
             )}
